e9b3f7c9479e560ca551b4ab08a4b012a76fc48a
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-11-04 15:16:46 +0200

    initial commit

---
+++ crud/crud/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin  
+from django.urls import path  
+from users import views
+
+
+urlpatterns = [  
+    path('admin/', admin.site.urls),  
+    path('', views.index),  
+    path('addnew',views.addnew),  
+    path('edit/<int:id>', views.edit),  
+    path('update/<int:id>', views.update),  
+    path('delete/<int:id>', views.destroy),  
+]  
